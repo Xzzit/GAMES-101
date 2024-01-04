d89ffd5d81541bdb120ec80ec149ca0c9219fd09
Xzzit <897177017@qq.com>
2024-01-04 14:47:56 +0900

    inderect lighting success!

--- homework7/Scene.cpp
+++ homework7/Scene.cpp
@@ -69,15 +69,21 @@ Vector3f Scene::castRay(const Ray &ray, int depth) const
     if (intersection.happened)
     {
         // Check if the intersection is with a light source
+
+        // If the intersection is with a light source, return the emission
         if (intersection.m->hasEmission())
         {
-            // If the intersection is with a light source, return the emission
             radiance = intersection.m->getEmission();
             return radiance;
         }
 
+        // If the intersection is not with a light source, then compute the radiance
         else
         {
+            // Initialize direct light and indirect light
+            Vector3f L_dir = Vector3f(0.0, 0.0, 0.0);
+            Vector3f L_indir = Vector3f(0.0, 0.0, 0.0);
+
             // Sample light source
             Intersection light_sample;
             float pdf_light;
@@ -88,10 +94,6 @@ Vector3f Scene::castRay(const Ray &ray, int depth) const
             Vector3f obj_light_direction = normalize(light_sample.coords - intersection.coords);
             Ray obj_light_ray = Ray(itersection_origin, obj_light_direction);
             Intersection obj_light_intersection = Scene::intersect(obj_light_ray);
-
-            // Initialize direct light and indirect light
-            Vector3f L_dir = Vector3f(0.0, 0.0, 0.0);
-            Vector3f L_indir = Vector3f(0.0, 0.0, 0.0);
 
             // If the ray is not blocked, then compute the direct light
             if (obj_light_intersection.happened && obj_light_intersection.m->hasEmission())
@@ -123,6 +125,7 @@ Vector3f Scene::castRay(const Ray &ray, int depth) const
 
                 // Compute the radiance
                 L_dir = L_i * BRDF * cos_theta * cos_theta_prime / pow(distance, 2) / pdf_light;
+                radiance += L_dir;
             }
 
             // [comment]
@@ -130,7 +133,32 @@ Vector3f Scene::castRay(const Ray &ray, int depth) const
             // formula: L_indir = castRay(new_ray, depth + 1) * BRDF * cos(theta) / pdf_hemi / P_RR
             // [comment]
 
-            radiance = L_dir + L_indir;
+            // Test Russian Roulette
+            float random = get_random_float();
+            if (random > RussianRoulette)
+            {
+                return radiance;
+            }
+
+            // Compute new ray direction
+            Vector3f new_ray_direction = intersection.m->sample(ray.direction, intersection.normal);
+
+            // Compute BRDF
+            Vector3f BRDF = intersection.m->eval(ray.direction, new_ray_direction, intersection.normal);
+
+            // Compute theta
+            float cos_theta = dotProduct(intersection.normal, new_ray_direction);
+
+            // Compute the radiance
+            Ray new_ray = Ray(intersection.coords + intersection.normal * EPSILON, new_ray_direction);
+
+            // Compute pdf_hemi
+            float pdf_hemi = intersection.m->pdf(ray.direction, new_ray_direction, intersection.normal);
+
+            // Compute the radiance
+            L_indir = castRay(new_ray, depth + 1) * BRDF * cos_theta / pdf_hemi / RussianRoulette;
+
+            radiance += L_indir;
         }
     }
 
